d1655928c24d8028dec6bd69621d5a1ea20528a7
Dagaet <alojera15@gmail.com>
2024-05-06 09:51:16 +0100

    Test(Green): textProcessing should count the number of words in the given text

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
-export function myFunction() {
-  return true
+export function textProcessing() {
+  const text = "Hello, this is an example for you to practice. You should grab this text and make it as your test case."
+  return "The text has in total " + text.split(" ").length + " words"
 }
